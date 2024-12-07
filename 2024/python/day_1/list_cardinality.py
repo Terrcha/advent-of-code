8bff52bbe3d33b9a078fa36e244fa20488b4b04f
Terry Chattaway <terry.chattaway.dev@pm.me>
2024-12-07 16:48:42 +0000

    Adding part two

---
+++ 2024/python/day_1/list_cardinality.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import polars as pl
+
+
+def main():
+    with open(Path.joinpath(Path(__file__).parent, "input.txt")) as _file:
+        data = {"col_1": [], "col_2": []}
+        for line in _file:
+            stripped = line.strip().replace(" ", ",").split(",")
+            first, last = stripped[0], stripped[-1]
+            (data["col_1"].append(int(first)),)
+            data["col_2"].append(int(last))
+
+        count = 0
+        df = pl.DataFrame(data["col_2"]).group_by("column_0").len().sort("len", descending=True)
+        for row in df.iter_rows():
+            if row[0] in data["col_1"]:
+                count += row[0] * row[1]
+
+        print(f"Final count is : {count}")
+                
+
+if __name__ == "__main__":
+    main()
